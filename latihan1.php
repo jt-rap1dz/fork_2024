5fc519b3509ec491f6c3f2c8663eb0434bde505c
username <akhmadarip8@gmail.com>
2024-07-01 12:11:39 +0700

    ubah file

--- latihan1.php
+++ latihan1.php
@@ -9,5 +9,7 @@ echo "Hello World!";
 // 3. git push -u origin master
 // push adalah perintah, origin adalah nama remote
 // master adalah nama branch
+//4. ini adalah modify dari saya
+//5. dengan menambahkan file baru
 
 ?>
