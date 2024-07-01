fc0f73108efd931179fa0a04370503578f9bc704
username <akhmadarip8@gmail.com>
2024-07-01 12:01:17 +0700

    ini file pertama

---
+++ latihan1.php
@@ -0,0 +1,13 @@
+<?php
+
+echo "Hello World!";
+//perintah untuk add atau tambah 
+//1. git add . atau git add nama_folder/file
+// perintah untuk commit atau komentar 
+// 2. git commit -m "komentarnya apa "
+// perinta untuk upload atau push 
+// 3. git push -u origin master
+// push adalah perintah, origin adalah nama remote
+// master adalah nama branch
+
+?>
